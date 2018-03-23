4ec0501ff078d0bd0010d7ecf7588aed217acdd2
Mike Raineri <michael.raineri@dell.com>
2018-03-23 10:24:49 -0400

    Corrected link to license in source files

--- redfishtoollib/__init__.py
+++ redfishtoollib/__init__.py
@@ -1,6 +1,6 @@
 # Copyright Notice:
 # Copyright 2016 Distributed Management Task Force, Inc. All rights reserved.
-# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Mockup-Creator/LICENSE.md
+# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Mockup-Creator/blob/master/LICENSE.md
 from .redfishtoolTransport import RfTransport
 
 
