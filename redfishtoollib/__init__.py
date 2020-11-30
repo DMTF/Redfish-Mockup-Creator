120869b99c04b554ae43c8c4a3e72c2219597b2d
Mike Raineri <michael.raineri@dell.com>
2020-11-30 12:53:06 -0500

    Genericized much of the tool and switched to leverage the python-redfish-library

--- redfishtoollib/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright Notice:
-# Copyright 2016 DMTF. All rights reserved.
-# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Mockup-Creator/blob/master/LICENSE.md
-from .redfishtoolTransport import RfTransport
-
-
-
